583487b69fc9629e7703e1083dcc45b8c3e3a79a
jnaustin2 <jnelson1787@gmail.com>
2026-07-10 12:43:14 -0700

    Implement Freedom Square landing page overhaul: new sections, naming system, unified CTAs

--- artifacts/freedom-square/src/App.tsx
+++ artifacts/freedom-square/src/App.tsx
@@ -1,13 +1,17 @@
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import { ProblemSection } from "@/components/ProblemSection";
+import { MemberTransformationSection } from "@/components/MemberTransformationSection";
 import { FreedomRequiresSection } from "@/components/FreedomRequiresSection";
 import { BenefitsSection } from "@/components/BenefitsSection";
 import { CommunityRhythmSection } from "@/components/CommunityRhythmSection";
+import { MonthlyFocusSection } from "@/components/MonthlyFocusSection";
+import { ProofOfLifeSection } from "@/components/ProofOfLifeSection";
 import { TeachingSection } from "@/components/TeachingSection";
+import { CommunityBelongingSection } from "@/components/CommunityBelongingSection";
 import { AudienceSection } from "@/components/AudienceSection";
-import { PullQuote } from "@/components/PullQuote";
 import { CultureSection } from "@/components/CultureSection";
+import { MemberProgressSection } from "@/components/MemberProgressSection";
 import { OfferSection } from "@/components/OfferSection";
 import { FAQSection } from "@/components/FAQSection";
 import { FinalCTA } from "@/components/FinalCTA";
@@ -21,12 +25,17 @@ export default function App() {
       <main>
         <Hero />
         <ProblemSection />
+        <MemberTransformationSection />
         <FreedomRequiresSection />
         <BenefitsSection />
         <CommunityRhythmSection />
+        <MonthlyFocusSection />
+        <ProofOfLifeSection />
         <TeachingSection />
+        <CommunityBelongingSection />
         <AudienceSection />
         <CultureSection />
+        <MemberProgressSection />
         <OfferSection />
         <FAQSection />
         <FinalCTA />
